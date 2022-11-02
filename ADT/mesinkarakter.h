5cba1f0faaf1652e5c28575527a1c64aa41c8d18
Hans Stephano <hans.stephano1912@gmail.com>
2022-11-02 14:38:22 +0700

    Added ADT mesinkarakter
    mesinkarakter.h & mesinkarakter.c

---
+++ ADT/mesinkarakter.h
@@ -0,0 +1,26 @@
+#ifndef __MESIN_KAR__
+#define __MESIN_KAR__
+
+#include "boolean.h"
+
+#define MARK '.'
+
+/* State Mesin */
+extern char CC;
+extern boolean EOP;
+
+void start();
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca. 
+   I.S. : sembarang
+   F.S. : cc adalah karakter pertama pada pita
+          Jika cc != MARK maka eop bernilai false 
+          Jika cc = MARK maka eop bernilai true */
+
+void adv();
+/* Pita dimajukan satu karakter.
+   I.S. : Karakter pada jendela = cc, cc != MARK 
+   F.S. : cc adalah karakter berikutnya dari cc yang lama,
+          cc mungkin = MARK
+          Jika cc = MARK maka eop bernilai true */
+
+#endif
